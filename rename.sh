65561c100d02576f80b4f2a7605b8e1ce87508cc
Lain <vvz.dev@gmail.com>
2022-02-06 22:49:10 +0300

    rename script now adds colors

--- rename.sh
+++ rename.sh
@@ -1,19 +1,20 @@
 #!/bin/zsh
-
 WALLPAPERS="./wallpapers"
+
+# Get main color from image
+function getcolor {
+    echo $(convert $1 +dither -resize 64x64 -colors 5 -define histogram:unique-colors=true -format "%c" histogram:info: | head -n 1 | grep -o '#......')
+}
 
 
 # for each image in wallpapers
 for IMG in `find $WALLPAPERS -name \*.jpg -or -name \*.png -or -name \*.jpeg`
 do
     SIZE=$(identify -format '%wx%h' $IMG)
-    NAME=$IMG:t:r
+    NAME=$(echo $IMG:t:r | sed -e 's/@.*//g')
     EXT=$IMG:t:e
+    COLOR=$(getcolor $IMG)
+
+    mv $IMG $WALLPAPERS/$NAME@$COLOR@$SIZE.$EXT
     
-    # if name is not like dawfawg@1920x1080.png
-    if [[ ! $NAME =~ .*@[0-9].*x[0-9].*$ ]]
-    then
-        # rename
-        mv $IMG $WALLPAPERS/$NAME@$SIZE.$EXT
-    fi
 done
