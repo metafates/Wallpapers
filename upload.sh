9f0d4250f3c2a8537db30c11ce6f301f05305ed9
Lain <vvz.dev@gmail.com>
2022-02-06 23:41:17 +0300

    medium folder added to script

--- upload.sh
+++ upload.sh
@@ -8,9 +8,11 @@ echo "Compressing images"
 
 echo "Pushing to GitHub"
 git rm -r --cached wallpapers --quiet
+git rm -r --cached medium --quiet
 git rm -r --cached compressed --quiet
 git add wallpapers 
 git add compressed
+git add medium
 git commit -am "[BOT] Wallpapers Updated" --quiet
 git push origin main --quiet
 echo "Images uploaded"
