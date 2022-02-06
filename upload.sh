47287c9f23ab5abbfa75db84e3c6de114a6ba9ae
Lain <vvz.dev@gmail.com>
2022-02-06 12:33:18 +0300

    [BOT] Wallpapers Updated

--- upload.sh
+++ upload.sh
@@ -6,8 +6,8 @@ echo "Compressing images"
 echo "Pushing to GitHub"
 git rm -r --cached wallpapers --quiet
 git rm -r --cached compressed --quiet
-git add wallpapers/* 
-git add compressed/*
+git add wallpapers 
+git add compressed
 git commit -am "[BOT] Wallpapers Updated" --quiet
 git push origin main --quiet
 echo "Images uploaded"
